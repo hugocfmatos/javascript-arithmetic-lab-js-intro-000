96448bc44d8e2a701d17d2c6b498f265c156f96f
hugo matos <hugocfmatos@gmail.com>
2020-09-09 19:45:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
+function makeInt(string) {
+  parseInt(string,10)
+  return string
+}
+
+function preserveDecimal(string) {
+  parseFloat(string,10)
+  return string
+}
